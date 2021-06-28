52ee785a3db6116e0672b768e729c1ca4276a2f0
Jake Appleby <jakeappleby@live.com>
2021-06-28 21:46:00 +0000

    Refactored function.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,4 @@
-const assertEqual = function (actual, expected) {
-  let p = 'Passed';
-  let f = 'Failed';
-  if (actual == expected) {
-    console.log(`The function has ${p} ✅`);
-  } else {
-    console.log(`The function has ${f} 🔻`);
-  }
-};
-
-const eqArrays = function (rOne, rTwo) {
-  // Loop through each array to check if it matches the opposing array's index values
-  // We want to ensure that we checked every item
-  if (rOne.length !== rTwo.length) {
-    return false;
-  }
-  for (var i = 0; i < rOne.length; i++) {
-    if (rOne[i] !== rTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arrayOne, arrayTwo) {
   let p = 'Passed';
@@ -33,21 +11,4 @@ const assertArraysEqual = function(arrayOne, arrayTwo) {
     console.log(`The function has ${f} 🔻`);
     }
 };
-
-
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, "1");
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([3, 2, 1], [1, 2, 3]), false); // => should PASS
-assertEqual(eqArrays([3, 2, 1], [3, 1, 3]), false); // => should PASS
-assertEqual(eqArrays(['3', '2', '1'], [3, 2, 1]), false); // => should PASS
+module.exports = assertArraysEqual;
