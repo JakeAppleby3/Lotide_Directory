2ceb64e651b0ff99d00404c0297332a6da71c1f0
Jake Appleby <jakeappleby@live.com>
2021-06-28 20:59:35 +0000

    Refactoring the eqArrays.js.

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-const assertEqual = function (actual, expected) {
-  let p = 'Passed';
-  let f = 'Failed';
-  if (actual == expected) {
-    console.log(`The function has ${p} ✅`);
-  } else {
-    console.log(`The function has ${f} 🔻`);
-  }
-};
-
 const eqArrays = function (actual, expected) {
   // Loop through each array to check if it matches the opposing array's index values
   // We want to ensure that we checked every item
@@ -19,20 +9,4 @@ const eqArrays = function (actual, expected) {
   return true;
 };
 
-
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, "1");
-
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([3, 2, 1], [1, 2, 3]), false); // => should PASS
-assertEqual(eqArrays([3, 2, 1], [3, 1, 3]), false); // => should PASS
-assertEqual(eqArrays(['3', '2', '1'], [3, 2, 1]), false); // => should PASS
+module.exports = eqArrays;
