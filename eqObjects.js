b1b48da1f4c15b3036379bd1c506c23585f5eaff
Jake Appleby <jakeappleby@live.com>
2021-06-29 05:53:09 +0000

    Updated version.

--- eqObjects.js
+++ eqObjects.js
@@ -1,27 +1,3 @@
-const eqArrays = function (actual, expected) {
-  // Loop through each array to check if it matches the opposing array's index values
-  // We want to ensure that we checked every item
-  if (actual.length !== expected.length) {
-    return false
-  }
-  for (var i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertEqual = function (actual, expected) {
-  let p = 'Passed';
-  let f = 'Failed';
-  if (actual == expected) {
-    console.log(`The function has ${p} ✅`);
-  } else {
-    console.log(`The function has ${f} 🔻`);
-  }
-};
-
 const eqObjects = function (object1, object2) {
   if (Object.keys(object1).length === Object.keys(object2).length) {
     for (let key of Object.keys(object1)) {
@@ -30,59 +6,10 @@ const eqObjects = function (object1, object2) {
           return false;
         }
       }
-
     }
     return true;
   }
   return false;
 };
 
-
-
-
-
-// // Returns true if both objects have identical keys with identical values.
-// // Otherwise you get back a big fat false!
-// const eqObjects = function (object1, object2) {
-//   let arrayOfKeys1 = Object.keys(object1);
-//   let arrayOfKeys2 = Object.keys(object2);
-//   if (arrayOfKeys1.length !== arrayOfKeys2.length) {
-//     return false;
-//   // // statement to determine if the object keys in object1 are the same as object2
-//   // if (Object.keys(object1).length !== Object.keys(object2).length) {
-//   //   // if not return the false
-//   //   return false;
-//   // }
-//   // loop through the keys in object one 
-//   }
-//     for (const key of arrayOfKeys1) {
-//       // statement to see if the keys hold the same values
-//       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-//         return eqArrays(object1[key], object2[key]); 
-//       } else {
-//         if (object1[key] !== object2[key]) {
-//         // return falsw if untrue
-//         return false;
-//       }
-//       }
-//     }
-//   return true;
-// };
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, "1");
-
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(assertEqual(eqObjects(ab, ba), true)); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-console.log(assertEqual(eqObjects(ab, abc), false)); // => false
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(assertEqual(eqObjects(cd, dc), true)); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(assertEqual(eqObjects(cd, cd2), false)); // => false
+module.exports = eqObjects;
