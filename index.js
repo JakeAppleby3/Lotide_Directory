eddda5e96722853892a5032f6a579e2d20e0b3d8
Jake Appleby <jakeappleby@live.com>
2021-06-29 06:09:18 +0000

    Updated index.

--- index.js
+++ index.js
@@ -5,12 +5,33 @@ const middle = require('./middle');
 const eqArrays = require("./eqArrays");
 const assertEqual = require("./assertEqual");
 const assertArraysEqual = require("./assertArraysEqual");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const countLetters = require("./countLetters");
+const eqObjects = require("./eqObjects");
+const findKey = require("./findKey");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const countOnly = require("./countOnly");
+const findKeyByValue = require("./findKeyByValue");
+const takeUntil = require("./takeUntil");
+const without = require("without");
+
+
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  eqArrays: eqArrays,
-  assertEqual: assertEqual,
-  assertArraysEqual: assertArraysEqual
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertEqual,
+  assertArraysEqual,
+  assertObjectsEqual,
+  countLetters,
+  eqObjects,
+  findKey,
+  letterPositions,
+  map,
+  countOnly,
+  findKeyByValue,
+  takeUntil,
 };
