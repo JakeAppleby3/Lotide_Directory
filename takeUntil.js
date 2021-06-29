eb3b93c4e6989e66cb4013c4b36a72d8a5994d5f
Jake Appleby <jakeappleby@live.com>
2021-06-29 05:26:10 +0000

    Updates version.

--- takeUntil.js
+++ takeUntil.js
@@ -1,6 +1,3 @@
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-
 const takeUntil = function (array, callback) {
   const result = [];
   for(let num of array) {
@@ -13,21 +10,5 @@ const takeUntil = function (array, callback) {
   }
   return result;
 };
-// slice of the array with elements taken from the beginning.
 
-const assertArraysEqual = function (actual, expected) {
-  if(actual.length !== expected.length) {
-    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
-    return;
-  }
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
-      return;
-    } 
-  }
-  console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
-};
-
-assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
-assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]);
+module.exports = takeUntil;
