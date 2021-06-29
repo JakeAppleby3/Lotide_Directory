9924c1e5d10f2064eaa3237b40a1cceb38b6f367
Jake Appleby <jakeappleby@live.com>
2021-06-29 02:04:09 +0000

    Updated version.

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,4 @@
 const eqArrays = function (actual, expected) {
-  // Loop through each array to check if it matches the opposing array's index values
-  // We want to ensure that we checked every item
   for(var i = 0; i < actual.length; i ++) {
     if (actual[i] !== expected[i]) {
       return false;
