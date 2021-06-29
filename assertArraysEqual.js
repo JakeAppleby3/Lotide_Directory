49e59c534fdd00b5bb5faaf1fde29a00c25f8051
Jake Appleby <jakeappleby@live.com>
2021-06-29 02:05:08 +0000

    Updated version.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,8 +3,6 @@ const eqArrays = require('./eqArrays');
 const assertArraysEqual = function(arrayOne, arrayTwo) {
   let p = 'Passed';
   let f = 'Failed';
-  // take in two arrays and console.log an apprpraite message to console
-  // use message from assertEqual
   if(eqArrays(arrayOne, arrayTwo)) {
     console.log(`The function has ${p} ✅`);
     } else {
